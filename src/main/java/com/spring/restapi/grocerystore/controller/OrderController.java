3085fde95f1375e7b1d0c4d9c7b41d5ec0f85d82
AyeshaFatima1 <98936880+AyeshaFatima1@users.noreply.github.com>
2022-06-06 18:16:29 +0530

    commit

---
+++ src/main/java/com/spring/restapi/grocerystore/controller/OrderController.java
@@ -0,0 +1,78 @@
+package com.spring.restapi.grocerystore.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import com.spring.restapi.grocerystore.dto.StripeResponse;
+import com.spring.restapi.grocerystore.exceptions.AuthenticationFailException;
+import com.spring.restapi.grocerystore.exceptions.OrderNotFoundException;
+import com.spring.restapi.grocerystore.model.Order;
+import com.spring.restapi.grocerystore.model.User;
+import com.spring.restapi.grocerystore.response.ApiResponse;
+import com.spring.restapi.grocerystore.service.AuthenticationService;
+import com.spring.restapi.grocerystore.service.OrderService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private AuthenticationService authenticationService;
+    
+    // place order after checkout
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> placeOrder(@RequestParam("token") String token, @RequestParam("sessionId") String sessionId)
+            throws AuthenticationFailException {
+        // validate token
+        authenticationService.authenticate(token);
+        // retrieve user
+        User user = authenticationService.getUser(token);
+        // place the order
+        orderService.placeOrder(user, sessionId);
+        return new ResponseEntity<>(new ApiResponse(true, "Order has been placed"), HttpStatus.CREATED);
+    }
+
+    // get all orders
+    @GetMapping("/")
+    public ResponseEntity<List<Order>> getAllOrders(@RequestParam("token") String token) throws AuthenticationFailException {
+        // validate token
+        authenticationService.authenticate(token);
+        // retrieve user
+        User user = authenticationService.getUser(token);
+        // get orders
+        List<Order> orderDtoList = orderService.listOrders(user);
+
+        return new ResponseEntity<>(orderDtoList, HttpStatus.OK);
+    }
+
+
+    // get orderitems for an order
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOrderById(@PathVariable("id") Integer id, @RequestParam("token") String token)
+            throws AuthenticationFailException {
+        // validate token
+        authenticationService.authenticate(token);
+        try {
+            Order order = orderService.getOrder(id);
+            return new ResponseEntity<>(order,HttpStatus.OK);
+        }
+        catch (OrderNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+}
